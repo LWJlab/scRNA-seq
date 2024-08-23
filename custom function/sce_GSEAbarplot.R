79395af3f7b18bb4b4245d5ddc66821b2ea8d0cd
Fanjie-Lin <149742959+LWJlab@users.noreply.github.com>
2024-08-23 13:20:44 +0800

    Update sce_GSEAbarplot.R

--- custom function/sce_GSEAbarplot.R
+++ custom function/sce_GSEAbarplot.R
@@ -469,8 +469,8 @@ sce_GSEAbarplot <- function(object, # A dataframe containing Pathway1, Pathway2 
     geom_text(aes(label = Label), 
               color = 'black',
               size = num_size,
-              hjust = 0.5,
-              nudge_x = 0) + 
+              hjust = 'left',
+              nudge_x = 0.1) + 
     scale_x_continuous(limits = xlim,
                        expand = expansion(mult = c(0, 0))) + 
     labs(x = "NES", y = "", title = title) +
