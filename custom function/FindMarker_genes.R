808bb01fa1696796cb76b3f002669c62d531c210
Fanjie-Lin <149742959+LWJlab@users.noreply.github.com>
2024-08-23 15:16:10 +0800

    Update FindMarker_genes.R

--- custom function/FindMarker_genes.R
+++ custom function/FindMarker_genes.R
@@ -1,4 +1,5 @@
-FindMarker_genes <- function(dataset, 
+FindMarker_genes <- function(object, 
+                             assay = NULL,
                              clusters, 
                              comparison,
                              ...) {
@@ -8,20 +9,20 @@ FindMarker_genes <- function(dataset,
     library(tidyverse)
     library(dplyr)})
 
-  Idents(dataset) <- paste(as.character(Idents(dataset)), dataset[[comparison[1]]][[1]], sep = "_")
+  Idents(object) <- paste(as.character(Idents(object)), object[[comparison[1]]][[1]], sep = "_")
   
   de <- list()
   
   for (i in seq_along(1:length(clusters))) {
     
-    d <- FindMarkers(dataset, 
+    d <- FindMarkers(object, 
                      ident.1 = paste(clusters[i], comparison[3], sep = "_"),
                      ident.2 = paste(clusters[i], comparison[2], sep = "_"),
-                     assay = "SCT",
+                     assay = assay,
                      ...)
     de[[i]] <- d %>%
-      rownames_to_column(var = "gene") %>%
-      add_column(cluster = clusters[i], .after = 1) 
+               rownames_to_column(var = "gene") %>%
+               add_column(cluster = clusters[i], .after = 1) 
     
     
   }
