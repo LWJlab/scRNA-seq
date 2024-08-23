73c86792d7efb9e2b8a6da1ca377c77a88213467
lwj-lab <149742959+Fanjie-Lin@users.noreply.github.com>
2024-08-23 09:21:21 +0800

    Create sce_GSEA.R

---
+++ custom function/sce_GSEA.R
@@ -0,0 +1,36 @@
+sce_GSEA <- function(diff, 
+                    clusters = NULL, 
+                    pathway) {
+  library(tidyverse)
+  if (!requireNamespace("fgsea", quietly = TRUE)) {
+    stop(paste("Package \"fgsea\" needed for this function to work. Please install it."),
+         call. = FALSE)
+  }
+  
+  if (is.null(clusters)) clusters <- unique(diff$cluster)
+  
+  gsea_res <- list()
+  
+  for (i in seq(length(clusters))) {
+    
+    data <- diff %>%
+      dplyr::filter(.data$cluster == clusters[i]) %>%
+      arrange(desc(.data$avg_log2FC))
+    
+    l <- data$avg_log2FC
+    names(l) <- data$gene
+    
+    res <- fgsea::fgsea(pathways = pathway, 
+                        stats = l, 
+                        minSize = 5, 
+                        maxSize = 500, 
+                        nperm = 10000)
+    
+    res <- res %>%
+      add_column(cluster = clusters[i],
+                 .before = 1)
+    
+    gsea_res[[i]] <- res
+  }
+  return(as_tibble(do.call("rbind", gsea_res)))
+}
