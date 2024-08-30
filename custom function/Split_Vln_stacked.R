161eb59eb9b139f053c8adaaf1e77c21c8dde88b
Fanjie-Lin <149742959+LWJlab@users.noreply.github.com>
2024-08-30 22:47:33 +0800

    Create Split_Vln_stacked.R

---
+++ custom function/Split_Vln_stacked.R
@@ -0,0 +1,114 @@
+Split_Vln_stacked <- function(object,#seurat对象
+                              assay,#矩阵
+                              feature,#需要展示的基因（一个向量）
+                              split.by=NULL,#split分组（两组）
+                              split.plot=NULL,#是否需要做split Violn
+                              pt.size, #点的大小（0表示不显示散点）
+                              size,#坐标轴轴的文字大小
+                              cols,#颜色设置（一个向量）
+                              test=T,#是否需要检验
+                              test_method = NULL,#c('t.test','wilcox.test'),#两组检验方法
+                              sig_label= NULL) {#标注*或者pvalue,, c("p.signif","p.format"))
+  
+  suppressPackageStartupMessages({
+    library(Seurat)
+    library(dittoSeq)
+    library(ggplot2)
+    library(ggpubr)})
+  
+  p1list <- list()
+  for (i in 1:length(feature)){
+    if(test==T){
+      p1 <- VlnPlot(object, assay = assay, features = feature[i], split.by = split.by,
+                    split.plot=split.plot, pt.size = pt.size)+
+            theme_bw()+
+            theme(axis.text = element_blank(),
+                  axis.title.x = element_blank(),
+                  axis.title.y = element_text(size = size, angle = 0, vjust = 0.5),
+                  axis.ticks = element_blank(),
+              title = element_blank(),
+              legend.position = 'none',
+              panel.border = element_rect(fill = NA),
+              plot.margin = margin(-0.05,0,-0.05,0, "cm"),
+              panel.grid = element_blank())+
+        ylab(feature[i])+
+        scale_fill_manual(values = cols)
+      p11 <- p1+ylim(0, max(p1$data[feature[i]]+1.5))+
+        stat_compare_means(aes(group = split),
+                           label =sig_label,
+                           label.y = max(p1$data[feature[i]]),
+                           hide.ns=T)
+      p1list[[i]] <- p11
+    }else{
+      p1 <- VlnPlot(object,  assay = assay, features = feature[i], split.by = split.by,
+                    split.plot=split.plot, pt.size = pt.size)+
+        theme_bw()+
+        theme(axis.text = element_blank(),
+              axis.title.x = element_blank(),
+              axis.title.y = element_text(size = size, angle = 0, vjust = 0.5),
+              axis.ticks = element_blank(),
+              title = element_blank(),
+              legend.position = 'none',
+              panel.border = element_rect(fill = NA),
+              plot.margin = margin(-0.05,0,-0.05,0, "cm"),
+              panel.grid = element_blank())+
+        ylab(feature[i])+
+        scale_fill_manual(values = cols)
+      p1list[[i]] <- p1
+    }
+  }
+  if(split.plot==T){
+    p2 <- VlnPlot(object,  assay = assay, features = tail(feature,1), split.by = split.by,
+                  split.plot=split.plot, pt.size = pt.size)+
+      theme_bw()+
+      theme(axis.text.y = element_blank(),
+            axis.text.x = element_text(colour = 'black', size = size, angle = 45, vjust = 1, hjust = 1),
+            axis.title.y = element_text(size = rel(1), angle = 0, vjust = 0.5),
+            axis.ticks = element_blank(),
+            title = element_blank(),
+            legend.position = 'bottom',
+            panel.border = element_rect(fill = NA),
+            plot.margin = margin(-0.05,0,0,0, "cm"),
+            panel.grid = element_blank(),
+            legend.box.background = element_blank(),
+            legend.text = element_text(color="black",size=10),
+            legend.spacing.x=unit(0.2,'cm'),
+            legend.key.width=unit(0.4,'cm'),
+            legend.key.height=unit(0.4,'cm'),
+            legend.background=element_blank())+
+      ylab(tail(feature,1))+
+      scale_fill_manual(values = cols)
+    p22 <- p2+ylim(0, max(p2$data[tail(feature,1)]+1.5))+
+      stat_compare_means(aes(group = split),
+                         label =sig_label,
+                         label.y = max(p2$data[tail(feature,1)]),
+                         hide.ns=T)
+  }else{
+    p2 <- VlnPlot(object,  assay = assay, features = tail(feature,1), split.by = split.by,
+                  split.plot=split.plot, pt.size = pt.size)+
+      theme_bw()+
+      theme(axis.text.y = element_blank(),
+            axis.text.x = element_text(colour = 'black', size = size, angle = 45, vjust = 1, hjust = 1),
+            axis.title.y = element_text(size = rel(1), angle = 0, vjust = 0.5),
+            axis.ticks = element_blank(),
+            title = element_blank(),
+            legend.position = 'none',
+            panel.border = element_rect(fill = NA),
+            plot.margin = margin(-0.05,0,0,0, "cm"),
+            panel.grid = element_blank())+
+      ylab(tail(feature,1))+
+      scale_fill_manual(values = cols)
+    p22 <- p2+ylim(0, max(p2$data[tail(feature,1)]+1.5))+
+      stat_compare_means(aes(group = split),
+                         label =sig_label,
+                         label.y = max(p2$data[tail(feature,1)]),
+                         hide.ns=T)
+  }
+  if(test==T){
+    p1list[[length(feature)]] <- p22
+  }else{
+    p1list[[length(feature)]] <- p2
+  }
+  p<- deeptime::ggarrange2(plots = p1list, nrow = length(feature))
+  return(p)
+}
